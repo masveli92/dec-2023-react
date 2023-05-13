f8624279b43f98a9c801ae430d7756e8d11a957d
masveli92 <masveli92@gmail.com>
2023-05-13 08:49:30 +0300

    lesson3 useForm, axios, validators

--- src/components/carForm/CarForm.js
+++ src/components/carForm/CarForm.js
@@ -1,11 +1,13 @@
 import {useForm} from "react-hook-form";
 import {useEffect} from "react";
+import {joiResolver} from "@hookform/resolvers/joi";
 
 import {carService} from "../../services/car.service";
+import {carValidator} from "../../validators/car.validator";
 
 
 const CarForm = ({setAllCars, carForUpdate, setCarForUpdate}) => {
-    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm( {mode:'all'});
+    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm( {mode:'all', resolver: joiResolver(carValidator)});
 
     useEffect(()=>{
         if (carForUpdate){
@@ -23,7 +25,7 @@ const CarForm = ({setAllCars, carForUpdate, setCarForUpdate}) => {
     }
 
     const update = async (car) => {
-        const {data}= await carService.updateById(carForUpdate?.id, car);
+        await carService.updateById(carForUpdate?.id, car);
         setAllCars(prev=>!prev);
         reset();
         setCarForUpdate(null)
@@ -32,31 +34,46 @@ const CarForm = ({setAllCars, carForUpdate, setCarForUpdate}) => {
 
 
     return (
+        // <form onSubmit={handleSubmit(carForUpdate?update:save)}>
+        //
+        //     <input type="text" placeholder={'brand'} {...register('brand', {
+        //         pattern: {
+        //             value: /^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/,
+        //             message: "this field must contain only letters, min-1, max-20"
+        //         },
+        //         required: {value:true, message: 'this field can`t be empty'}
+        //     })}/>
+        //     {errors.brand && <span>{errors.brand.message}</span>}
+        //
+        //     <input type="text" placeholder={'price'} {...register('price', {
+        //         valueAsNumber: true,
+        //         min: {value: 0, message: 'min 0'},
+        //         max: {value: 1000000, message: "max 1000000"},
+        //         required: {value:true, message: 'this field can`t be empty'}
+        //     })}/>
+        //     {errors.price && <span>{errors.price.message}</span>}
+        //
+        //     <input type="text" placeholder={'year'} {...register('year', {
+        //         valueAsNumber: true,
+        //         min: {value: 1990, message: 'not older then 1990'},
+        //         max: {value: new Date().getFullYear(), message: 'not younger then current year'},
+        //         required: {value:true, message: 'this field can`t be empty'}
+        //     })}/>
+        //     {errors.year && <span>{errors.year.message}</span>}
+        //
+        //     <button disabled={!isValid}>{carForUpdate?'Update':'Save'}</button>
+        //
+        // </form>
+
         <form onSubmit={handleSubmit(carForUpdate?update:save)}>
 
-            <input type="text" placeholder={'brand'} {...register('brand', {
-                pattern: {
-                    value: /^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/,
-                    message: "this field must contain only letters, min-1, max-20"
-                },
-                required: {value:true, message: 'this field can`t be empty'}
-            })}/>
+            <input type="text" placeholder={'brand'} {...register('brand')}/>
             {errors.brand && <span>{errors.brand.message}</span>}
 
-            <input type="text" placeholder={'price'} {...register('price', {
-                valueAsNumber: true,
-                min: {value: 0, message: 'min 0'},
-                max: {value: 1000000, message: "max 1000000"},
-                required: {value:true, message: 'this field can`t be empty'}
-            })}/>
+            <input type="text" placeholder={'price'} {...register('price')}/>
             {errors.price && <span>{errors.price.message}</span>}
 
-            <input type="text" placeholder={'year'} {...register('year', {
-                valueAsNumber: true,
-                min: {value: 1990, message: 'not older then 1990'},
-                max: {value: new Date().getFullYear(), message: 'not younger then current year'},
-                required: {value:true, message: 'this field can`t be empty'}
-            })}/>
+            <input type="text" placeholder={'year'} {...register('year')}/>
             {errors.year && <span>{errors.year.message}</span>}
 
             <button disabled={!isValid}>{carForUpdate?'Update':'Save'}</button>
